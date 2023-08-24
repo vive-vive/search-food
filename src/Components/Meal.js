7be54934720bb13cf214c274a6899317841879c5
vive-vive <vivekaeswaramoorthy@gmail.com>
2023-08-24 11:09:28 +0530

    commit

---
+++ src/Components/Meal.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import Mealltem from "./Mealltem";
+import './style.css';
+const Meal = () => {
+    const[search,setSearch]=useState("");
+    const[Mymeal,setMeal]=useState();
+    const searchMeal=(evt)=>{
+        if(evt.key=="Enter")
+        {
+            fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`).then(res=>res.json()).then(data=> {setMeal(data.meals);setSearch("")})
+        }
+    }
+    return (
+        <>
+            <div className="main">
+                <div className="heading">
+                    <h1>Hey Folks!!!</h1>
+                    <h4>Foodie adventures are the best adventures Let's explore them!</h4>
+                </div>
+                <div className="searchBox">
+                    <input type="search" className="search-bar" onChange={(e)=>setSearch(e.target.value)} value={search} onKeyPress={searchMeal}/>
+                </div>
+                <div className="container">
+                   {   
+                  
+                    (Mymeal==null)? <p className="notSearch">Not found</p> : 
+                         Mymeal.map((res)=>{
+                             return(
+                            <Mealltem data={res}/>)} 
+                     
+                    ) 
+                   
+                   }
+                </div>
+            </div>
+        </>
+    )
+}
+export default Meal;
